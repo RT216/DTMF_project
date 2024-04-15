0db4bdda612313188b6030524568bf571d2ce91e
雪野深月 <richtang1216@outlook.com>
2024-04-15 17:31:54 +0800

    [add] complete generate_LUT.m

---
+++ LUT_generation/generate_LUT.m
@@ -0,0 +1,49 @@
+%**********************************************************************
+%  Project: DTMF
+%  File: generate_LUT.m
+%  Description: generate LUT for sin(), and store it in hex format
+%               The LUT is generated for the first quadrant of sin() 
+%               and then mirrored to the other quadrants.
+%  Author: Ruiqi Tang
+%  Timestamp:
+%----------------------------------------------------------------------
+% Code Revision History:
+% Ver:     | Author    | Mod. Date     | Changes Made:
+% v1.0.0   | R.T.      | 2024/04/15    | Initial version
+%**********************************************************************
+clear; clc; close all;
+
+N = 100; % resolution of the LUT
+x = linspace(0, pi/2, N); % generate N equally spaced points between -pi and pi
+
+% calculate the sin value for each x and store them in an array
+sinLUT = sin(x);
+
+% map the sin value to 12-bit unsigned integer
+sinLUT = round(sinLUT * (2^12-1));
+
+% write the LUT to a file in hex format
+fileID = fopen('../sinLUT.hex', 'w');
+for i = 1:N
+    fprintf(fileID, '%04X\n', sinLUT(i));
+end
+fclose(fileID);
+
+
+% demo of read and plot the LUT
+fileID = fopen('../sinLUT.hex', 'r');
+sinLUT = fscanf(fileID, '%x');
+fclose(fileID);
+
+sinLUTr = zeros(1, N*4);
+sinLUTr(1:N) = sinLUT;
+sinLUTr(N+1:2*N) = sinLUT(end:-1:1);
+sinLUTr(2*N+1:3*N) = -sinLUT;
+sinLUTr(3*N+1:4*N) = -sinLUT(end:-1:1);
+
+figure;
+plot(sinLUTr);
+title('LUT for sin()');
+xlabel('Index');
+ylabel('Value');
+grid on;
